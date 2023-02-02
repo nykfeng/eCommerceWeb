cdbd6d31fb1d696066e73a9aedfc66e608ee96a8
Lifeng Huang <nykfengcode@gmail.com>
2023-02-01 22:31:34 -0500

    Add Entity Framework connection, migration and SQL Server DB

---
+++ eCommerceWebProj/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using eCommerceWebProj.Data;
+using eCommerceWebProj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceWebProj.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ eCommerceWebProj/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerceWebProj.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceWebProj.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
