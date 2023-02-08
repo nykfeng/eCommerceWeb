0d2b8c9b36d7e8659653bdfdcf3d8e04a3212d33
Lifeng Huang <nykfengcode@gmail.com>
2023-02-07 23:28:46 -0500

    Add CRUD functionalities and views

--- eCommerceWebProj/Controllers/CategoryController.cs
+++ eCommerceWebProj/Controllers/CategoryController.cs
@@ -17,5 +17,107 @@ namespace eCommerceWebProj.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Display Order can't be the same as Name");
+                // This here with Name, will be a specific error to the Name label
+                ModelState.AddModelError("Name", "Display Order can't be the same as Name");
+            }
+            // in .NET Core we have this method to check validation
+            if (ModelState.IsValid)
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
+            return RedirectToAction("Index");
+            } 
+            return View(obj);
+        }
+
+
+		// GET with Edit using ID
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { 
+            return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb== null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST with Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Display Order can't be the same as Name");
+                // This here with Name, will be a specific error to the Name label
+                //ModelState.AddModelError("Name", "Display Order can't be the same as Name");
+            }
+            // in .NET Core we have this method to check validation
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET using ID
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST with Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id); 
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
+            
+        }
     }
 }
--- eCommerceWebProj/Models/Category.cs
+++ eCommerceWebProj/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerceWebProj.Models
 {
@@ -7,7 +8,12 @@ namespace eCommerceWebProj.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; }
+        
+        // using this following attribute, we can display the name with space 
+        // instead of the default one without space
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be 1 and 100")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
